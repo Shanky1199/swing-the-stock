8f5394ac62e3b75e2cc264ed715eca76e30f36b5
Shashank <shashanks939@gmail.com>
2023-11-24 08:36:59 +0530

    adding all the controller setup

---
+++ app/controllers/recommendation_controller.py
@@ -0,0 +1,19 @@
+from services.recommendation_service import RecommendationService
+
+def get_recommendations_controller():
+    # Logic to get stock recommendations
+    pass
+
+def add_recommendation_controller(data):
+    # Logic to add a new recommendation
+    pass
+
+def update_recommendation_controller(recommendation_id, data):
+    # Logic to update an existing recommendation
+    pass
+
+def delete_recommendation_controller(recommendation_id):
+    # Logic to delete a recommendation
+    pass
+
+# Additional recommendation-related controller functions can be added here...
---
+++ app/controllers/trade_controller.py
@@ -0,0 +1,19 @@
+from services.trade_service import TradeService
+
+def execute_trade_controller(data):
+    # Logic to execute a trade
+    pass
+
+def trade_history_controller(user_id):
+    # Logic to retrieve trade history
+    pass
+
+def update_trade_controller(trade_id, data):
+    # Logic to update an existing trade
+    pass
+
+def cancel_trade_controller(trade_id):
+    # Logic to cancel a trade
+    pass
+
+# Additional trade-related controller functions can be added here...
