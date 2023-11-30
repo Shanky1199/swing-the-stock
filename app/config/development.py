5a01fbfb3e236f01fbe201e32200fec11a23b713
Shashank <shashanks939@gmail.com>
2023-11-30 13:46:54 +0530

    changes on config and on basic-setup

--- app/config/development.py
+++ app/config/development.py
@@ -1,6 +1,5 @@
-from .default import Config
+from . import Config
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'your_development_database_uri'
-    # Other development-specific settings...
+    SQLALCHEMY_DATABASE_URI = 'postgresql://tradinguser:password@localhost/tradingapp'
